69d0c1759c9db506de3745ecef2a22425ae0bd9a
FlaviusBanned <am_ban_pe_github@yahoo.com>
2024-12-13 00:04:41 +0200

    13.12.2024 12:05

---
+++ script_java/data.js
@@ -0,0 +1,11 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyB6LGyoosFP5HwgzqdoJVh797Qhwv8Fzlg",
+    authDomain: "ase-storage.firebaseapp.com",
+    databaseURL: "https://ase-storage-default-rtdb.firebaseio.com",
+    projectId: "ase-storage",
+    storageBucket: "ase-storage.firebasestorage.app",
+    messagingSenderId: "56574862198",
+    appId: "1:56574862198:web:f9063396b80ede2eca5057",
+    measurementId: "G-51J6SN1SWR"
+  };
